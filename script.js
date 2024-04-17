0d81157d8720625b12e70e614f9dd0114871eca3
codersaradhi <chaitanyasaradhi1999@gmail.com>
2024-04-17 16:53:28 +0530

    js commint

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const listsContainer = document.querySelector('[data-lists]');
 const newListForm = document.querySelector('[data-new-list-form]');         
 const newListInput = document.querySelector('[data-new-list-input]');
-const deleteListButton = document.querySelector(['data-delete-list-button']);         
+const deleteListButton = document.querySelector('[data-delete-list-button]');         
 
 const LOCAL_STORAGE_LIST_KEY = 'task.lists'; //storing the list values in the localstroge of browser
 const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedlistId'; //to know which item is selected
 let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
 let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
+
 
 
 listsContainer.addEventListener('click',e =>{
@@ -16,22 +17,24 @@ listsContainer.addEventListener('click',e =>{
     }
 })
 
-// deleteListButton.addEventListener('click', e =>{
-//     lists = lists.filter(list => list.id !== selectedListId);
-//     selectedListId = null;
-//     saveAndrender();
-// })y
+
+deleteListButton.addEventListener('click',e =>{
+    lists = lists.filter(list => list.id !== selectedListId);
+    selectedListId = null;
+    saveAndrender();
+});
 
 
 newListForm.addEventListener('submit', e =>{
     e.preventDefault();
     const listName =  newListInput.value;
+    lists.push(list);
     if(listName == null || listName === '') return;
     const list = createList(listName);
     newListInput.value = null; //gives the empty todo
     lists.push(list);
     saveAndrender();
-
+    
 })
 
 function createList(name){
